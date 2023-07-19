7388c6cdfd4a4a41f52dc81544aacbdfedadf1a1
usmfi <usmfi@student.kit.edu>
2023-07-19 16:25:47 +0200

    added initial position & extrusion in self test

--- pyGCodeDecode/test/test_state_generator.py
+++ pyGCodeDecode/test/test_state_generator.py
@@ -14,13 +14,17 @@ def test_state_generator():
         printer="test",
         layer_cue="LAYER_CHANGE",
     )
+
+    initial_pos = (5, 4, 3, 2)  # initial position definition
+    test_setup.set_initial_position(*initial_pos)
+
     states = state_generator(
         filename=os.path.abspath("pyGCodeDecode/test/test_state_generator.gcode"),
         initial_machine_setup=test_setup.get_dict(),
     )
-    # print(states[0])
-    # assert True
-    assert isinstance(states, list)
-    assert states[0].state_position.get_vec(withExtrusion=True) == [0, 0, 0, 0]
-    assert states[1].state_position.get_vec(withExtrusion=True) == [10, 20, 30, 0]
-    assert states[2].state_position.get_vec(withExtrusion=True) == [300, 200, 100, 0]
+
+    assert isinstance(states, list)  # check if state list gets generated
+
+    assert states[0].state_position.get_vec(withExtrusion=True) == list(initial_pos)  # test for inital position
+    assert states[1].state_position.get_vec(withExtrusion=True) == [10, 20, 30, initial_pos[-1]]  # test for second pos
+    assert states[2].state_position.get_vec(withExtrusion=True) == [300, 200, 100, 50]  # test for another pos with extr
