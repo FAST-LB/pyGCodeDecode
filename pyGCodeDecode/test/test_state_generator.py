2527c72e8a27b40509912c2b2cb6009bbd693a83
usmfi <usmfi@student.kit.edu>
2023-07-19 16:01:41 +0200

    path update

--- pyGCodeDecode/test/test_state_generator.py
+++ pyGCodeDecode/test/test_state_generator.py
@@ -10,12 +10,12 @@ def test_state_generator():
     from pyGCodeDecode.gcode_interpreter import setup
 
     test_setup = setup(
-        filename=os.path.abspath(r".\pyGCodeDecode\test\test_state_generator_setup.yaml"),
+        filename=os.path.abspath(r"pyGCodeDecode\test\test_state_generator_setup.yaml"),
         printer="test",
         layer_cue="LAYER_CHANGE",
     )
     states = state_generator(
-        filename=os.path.abspath(r".\pyGCodeDecode\test\test_state_generator.gcode"),
+        filename=os.path.abspath(r"pyGCodeDecode\test\test_state_generator.gcode"),
         initial_machine_setup=test_setup.get_dict(),
     )
     # print(states[0])
