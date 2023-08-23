21f60d7b3ef612a29be89dad716d55ed85b29d61
usmfi <usmfi@student.kit.edu>
2023-08-23 14:43:33 +0200

    failed segments in test.gcode +  cleanup

--- example/Restore/call.py
+++ example/Restore/call.py
@@ -1,17 +1,36 @@
 # -*- coding: utf-8 -*-
-"""Example usage of pyGCD."""
-from pyGCodeDecode import gcode_interpreter
+"""Example usage of pyGCD in the Restore Project."""
+from pyGCodeDecode import abaqus_file_generator, gcode_interpreter  # noqa F401
 
-setup = gcode_interpreter.setup(filename=r"example\printer_presets.yaml", printer="anisoprint_A4")
+setup = gcode_interpreter.setup(
+    filename=r"example\printer_presets.yaml", printer="anisoprint_A4"
+)  # Select printer from preset.
 setup.set_property({"layer_cue": "LAYER_CHANGE"})  # Prusa Slicer layer change cue.
 
-new = gcode_interpreter.simulate(filename=r"example\Restore\dwell_test.gcode", initial_machine_setup=setup)
+stator_simulation = gcode_interpreter.simulate(
+    filename=r"example\Restore\stator.gcode", initial_machine_setup=setup
+)  # Simulate the gcode.
 
 
-print(new.states)
+# Write all pause times and total printing time into seperate file.
+p_log = open("./example/Restore/pause_log.txt", "w")
+p_log.write("total time " + str(stator_simulation.blocklist[-1].segments[-1].t_end) + "\n")
 
-# new.plot_vel(show=True, filename=False)
-# new.save_summary()
+stator_simulation.plot_3d_mayavi()
 
-new.plot_vel(show=True, filename=False)
-# new.plot_3d_mayavi(extrusion_only=False)
+# for block in stator_simulation.blocklist:
+#     if block.state_B.pause is not None:
+#         # print("Pause ---> " + str(block.state_B.comment))
+#         # print("pause start: " + str(block.segments[0].t_begin))
+#         # print("pause   end: " + str(block.segments[-1].t_end))
+#         # print("Z: " + str(block.segments[0].pos_begin.z) + "\n")
+
+#         p_log.write("Pause ---> " + str(block.state_B.comment))
+#         p_log.write("\tpause start: " + str(block.segments[0].t_begin))
+#         p_log.write("\tpause start+5: " + str(block.segments[0].t_begin + 5))
+#         p_log.write("\tpause end: " + str(block.segments[-1].t_end))
+#         p_log.write("\tpause dur: " + str(block.segments[-1].t_end - block.segments[0].t_begin))
+#         p_log.write("\tZ: " + str(block.segments[0].pos_begin.z) + "\n")
+# p_log.close()
+
+# abaqus_file_generator.generate_abaqus_events(stator_simulation, "./example/Restore/stator_event_series.inp")
