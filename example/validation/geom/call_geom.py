3b250e7abbde4b3f771596513dd73f648d57ce12
usmfi <usmfi@student.kit.edu>
2023-11-24 15:18:56 +0100

    added more example & validation plots

--- example/validation/geom/call_geom.py
+++ example/validation/geom/call_geom.py
@@ -2,21 +2,21 @@
 """Example usage of pyGCD in the Restore Project."""
 import time
 
-from pyGCodeDecode import abaqus_file_generator, gcode_interpreter  # noqa F401
+from pyGCodeDecode import gcode_interpreter  # noqa F401
 from pyGCodeDecode.tools import print_layertimes
 
 start_time = time.time()
 
 setup = gcode_interpreter.setup(filename=r"example\printer_presets.yaml")  # load setup
-setup.select_printer("prusa_mini")  # Select printer from preset.
+setup.select_printer("prusa_mini_klipper")  # Select printer from preset.
 setup.set_property({"layer_cue": "LAYER_CHANGE"})  # Prusa Slicer layer change cue.
 
-setup.set_property({"firmware": "marlin_jerk"})
 simulation = gcode_interpreter.simulate(
-    filename=r"example\validation\jerk\JDJERK_1_5_7_10_15_20_30.gcode", initial_machine_setup=setup
+    filename=r"example\validation\geom\iso10circum_1000.gcode", initial_machine_setup=setup
 )  # Simulate the gcode.
 
 print("---Simulation took %s seconds ---" % (time.time() - start_time))
-print_layertimes(simulation=simulation, filename="./example/validation/jerk/layertime.csv")
+print_layertimes(simulation=simulation, filename="example/validation/geom/layertime.csv")
 
-simulation.plot_vel(show=True, filename=False)
+# simulation.plot_vel(show=True, filename=False)
+simulation.plot_3d_mayavi()
