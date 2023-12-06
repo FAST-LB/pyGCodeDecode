ce3a403af1bf6468465e0a1328aa13b9adc68486
usmfi <usmfi@student.kit.edu>
2023-12-06 15:11:09 +0100

    fix example to new Plot

--- example/dropshape/call.py
+++ example/dropshape/call.py
@@ -12,5 +12,5 @@ setup.set_initial_position(89.964, 78.843, 0.0, 0.0)
 new = gcode_interpreter.simulate(filename=r"example\dropshape\dropshape.gcode", initial_machine_setup=setup)
 # print(new.states)
 
-new.plot_vel(show=True, filename=False)
+new.plot_vel(show=True)
 new.plot_3d_mayavi(extrusion_only=False)
