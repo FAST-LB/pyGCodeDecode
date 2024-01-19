5fe76774e371aed66c7a58e262d1252b7692a25a
Lukas Hof <lukas.hof@kit.edu>
2024-01-19 11:48:09 +0100

    forgot some comments

--- examples/benchy/benchy.py
+++ examples/benchy/benchy.py
@@ -7,16 +7,18 @@ from pyGCodeDecode.gcode_interpreter import setup, simulation
 if __name__ == "__main__":
     script_dir = pathlib.Path(__file__).parent.resolve()
 
+    # setting up the printer
     printer_setup = setup(
         filename=pathlib.Path(script_dir) / "data" / "printer_presets.yaml",
         printer="prusa_mini",
         layer_cue="LAYER_CHANGE",
     )
 
+    # running the simulation by creating a simulation object
     simulation = simulation(
         filename=pathlib.Path(script_dir) / "data" / "Benchy.gcode",
         initial_machine_setup=printer_setup,
     )
 
-    # creating a 3D-plot
+    # create a 3D-plot
     simulation.plot_3d(extrusion_only=True)
