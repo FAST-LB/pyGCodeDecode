e8eedef32982edce3ab5621cf95f37e99d258a24
Lukas Hof <lukas.hof@kit.edu>
2024-01-19 15:38:55 +0100

    adding a very simple example using the brace

---
+++ examples/brace/brace.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Minimum example simulating the G-code of a brace from Aura-slicer on an Anisoprint Composer A4."""
+import pathlib
+
+from pyGCodeDecode.gcode_interpreter import simulation
+
+if __name__ == "__main__":
+    script_dir = pathlib.Path(__file__).parent.resolve()
+
+    # running the simulation by creating a simulation object using default machine parameters
+    brace_simulation = simulation(
+        filename=pathlib.Path(script_dir) / "data" / "brace.gcode", machine_name="anisoprint_a4"
+    )
+
+    # create a 3D-plot
+    brace_simulation.plot_3d(extrusion_only=True)
