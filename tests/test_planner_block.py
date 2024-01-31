7c3870e260604927fc33705295e939fa87363555
usmfi <usmfi@student.kit.edu>
2024-01-31 19:47:06 +0100

    make lint happyyyyy

--- tests/test_planner_block.py
+++ tests/test_planner_block.py
@@ -45,9 +45,7 @@ def test_planner_block():
     dist = 30
     pos_0 = position(0, 0, 0, 0)
     pos_1 = position(dist, 0, 0, 0)
-    settings = state.p_settings(
-        p_acc=100, jerk=0, vX=100, vY=100, vZ=100, vE=100, speed=100, units="SImm"
-    )
+    settings = state.p_settings(p_acc=100, jerk=0, vX=100, vY=100, vZ=100, vE=100, speed=100, units="SImm")
     state_0 = state(state_position=pos_0, state_p_settings=settings)
     state_1 = state(state_position=pos_1, state_p_settings=settings)
     state_1.prev_state = state_0
@@ -69,9 +67,7 @@ def test_planner_block():
     pos_1 = position(dist, 0, 0, 0)
     pos_2 = position(dist * 2, 0, 0, 0)
 
-    settings = state.p_settings(
-        p_acc=100, jerk=10, vX=100, vY=100, vZ=100, vE=100, speed=100, units="SImm"
-    )
+    settings = state.p_settings(p_acc=100, jerk=10, vX=100, vY=100, vZ=100, vE=100, speed=100, units="SImm")
     state_0 = state(state_position=pos_0, state_p_settings=settings)
     state_1 = state(state_position=pos_1, state_p_settings=settings)
     state_2 = state(state_position=pos_2, state_p_settings=settings)
